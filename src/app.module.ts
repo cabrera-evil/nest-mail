8c0174b5d30e5aa070756ed087168a4b7e0dc5c2
cabrera-evil <douglascabrera.dev@gmail.com>
2023-10-01 16:07:39 -0600

    Delete: app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppConfig } from './config/app';
 import { MailModule } from './modules/mail/mail.module';
 
 @Module({
   imports: [AppConfig, MailModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
